22cd55cedcab18153f394a50cd24edb034ba6e7a
unknown <ryleymcc@shaw.ca>
2021-09-19 14:05:59 -0500

    fix

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -13,7 +13,7 @@ class CarController():
     """ Controls thread """
 
     can_sends = []
-
+    
     ### STEER ###
 
     if enabled:
@@ -27,7 +27,7 @@ class CarController():
         apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last,
                                                     CS.out.steeringTorque, CarControllerParams)
       self.steer_rate_limited = new_steer != apply_steer
-
+      self.apply_steer_last = apply_steer
       if CS.out.standstill and frame % 5 == 0:
         # Mazda Stop and Go requires a RES button (or gas) press if the car stops more than 3 seconds
         # Send Resume button at 20hz if we're engaged at standstill to support full stop and go!
@@ -36,12 +36,12 @@ class CarController():
     else:
       apply_steer = 0
       self.steer_rate_limited = False
-      if CS.out.cruiseState.enabled and frame % 20 == 0:
+      #if CS.out.cruiseState.enabled and frame % 20 == 0:
         # Cancel Stock ACC if it's enabled while OP is disengaged
         # Send at a rate of 5hz until we sync with stock ACC state
-        #can_sends.append(mazdacan.create_button_cmd(self.packer, CS.CP.carFingerprint, Buttons.CANCEL))
+        # can_sends.append(mazdacan.create_button_cmd(self.packer, CS.CP.carFingerprint, Buttons.CANCEL))
 
-    self.apply_steer_last = apply_steer
+    
     
     #if ti is enabled we don't have to send apply steer to the stock system but a signal should still be sent.
     if CS.CP.enableTorqueInterceptor:
